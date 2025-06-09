0104b83499b6f104c1c52025fab2d4412ddd3258
malina997 <jakub.jandik@student.sps-cl.cz>
2025-06-09 09:51:43 +0200

    added: server files

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const apiRoutes = require('./api/api');
+app.use('/api', apiRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
